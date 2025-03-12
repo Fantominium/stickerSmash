6cb67cb26d68689a5428aaf77c41b979376001cc
Malcolm Garner <malcolm.garner4@gmail.com>
2025-03-12 22:05:44 +0000

    screenshotting on mobile and web

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,8 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Platform } from "react-native";
 import Button  from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
 import * as ImagePicker from 'expo-image-picker';
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { type ImageSource } from 'expo-image';
 import CircleButton from "@/components/CircleButton";
 import IconButton from "@/components/IconButton";
@@ -10,6 +10,10 @@ import EmojiPicker from "@/components/EmojiPicker";
 import EmojiList from "@/components/EmojiList";
 import EmojiSticker from "@/components/EmojiSticker";
 import {GestureHandlerRootView} from "react-native-gesture-handler";
+import * as MediaLibrary from 'expo-media-library';
+import { captureRef } from 'react-native-view-shot';
+import domtoimage from 'dom-to-image';
+
 
 const placeholderImage = require ("../../assets/images2/background-image.png");
 
@@ -18,8 +22,13 @@ export default function Index() {
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
   const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
   const [pickedEmoji, setPickedEmoji] = useState<ImageSource | undefined>(undefined);
+  const [status, requestPermission] = MediaLibrary.usePermissions();
+  const imageRef = useRef<View>(null);
 
 
+  if (status === null) {
+    requestPermission();
+  }
   const onReset = () => {
     setShowAppOptions(false);
   };
@@ -31,7 +40,37 @@ export default function Index() {
     setIsModalVisible(false);
   };
   const onSaveImageAsync = async () => {
-    // we will implement this later
+    if (Platform.OS !== 'web'){
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
+
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert('Saved!');
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    } else {      
+        try {
+          //needed to cast this ref here as node was not assignable to type View or null
+        const dataUrl = await domtoimage.toJpeg(imageRef.current as unknown as Node, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement('a');
+        link.download = 'sticker-smash.jpeg';
+        link.href = dataUrl;
+        link.click();
+      } catch (e) {
+        console.log(e);
+      }
+  }
   };
 
   const pickImageAsync = async () => {
@@ -53,16 +92,18 @@ export default function Index() {
       style={styles.container}
     >
       <View style={styles.imageContainer}>
-        <ImageViewer
-          imgSource={placeholderImage}
-          selectedImage={image}
-        />
-        {pickedEmoji && (
-          <EmojiSticker
-            imageSize={40}
-            stickerSource={pickedEmoji}
+        <View ref={imageRef} collapsable={false}>
+          <ImageViewer
+            imgSource={placeholderImage}
+            selectedImage={image}
           />
-        )}
+          {pickedEmoji && (
+            <EmojiSticker
+              imageSize={40}
+              stickerSource={pickedEmoji}
+            />
+          )}
+          </View>
         </View>
         {showAppOptions ? (
         <View style={styles.optionsContainer}>
